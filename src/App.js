3bd4089a1a65cc12623c994d7d9c91676d09e64f
kkyrylov <kkyrylov@softserveinc.com>
2021-09-22 03:33:27 +0300

    added css support via webpack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
+import "./index.css";
+
 
 class App extends Component{
   render(){
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
 					presets: ["@babel/env", ]
 				}
 			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
 		]
 	},
 	// plugins section
