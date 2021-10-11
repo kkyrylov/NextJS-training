6b3fefda6a4d7581058c01ba6a006a7bb6e2be2b
Kyryl Kyrylov <kyrylkyrylov@Kyryls-MacBook-Pro.local>
2021-10-11 20:08:05 +0300

    added mode, added devDeps

--- webpack.config.js
+++ webpack.config.js
@@ -4,69 +4,70 @@ const webpack = require("webpack");
 
 
 module.exports = {
-	// entry point of application
-	entry: './src/index.js',
-	// output path for bundled files
-	output: {
-		// path for the built project code
-		path: path.resolve(__dirname, 'dist'),
-		// name for the output file
-		filename: 'index.js'
-	},
-	// loaders section
-	module: {
-		rules: [
-			// JS Files
-			{
-				test: /\.(js|jsx|ts|tsx)$/,
-				include: path.resolve(__dirname, 'src'),
-				exclude: path.resolve(__dirname, 'node_modules'),
-				loader: "babel-loader",
-				options: {
-					presets: ["@babel/env", ],
-				}
-			},
-			// Css files works with modules and plain css
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true
-						}
-					}
-				],
-				include: /\.module\.css$/
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				],
-				exclude: /\.module\.css$/
-			},
-		]
-	},
-	// plugins section
-	plugins: [
-		// eslint plugin
-		new ESLintPlugin({
-			exclude: [
-				path.resolve(__dirname, 'node_modules'),
-				path.resolve(__dirname, 'dist'),
-			],
-			failOnError: true,
-			fix: true,
-			extensions: ['js', 'jsx', 'ts', 'tsx']
-		}),
+  mode: 'development',
+  // entry point of application
+  entry: './src/index.js',
+  // output path for bundled files
+  output: {
+    // path for the built project code
+    path: path.resolve(__dirname, 'dist'),
+    // name for the output file
+    filename: 'index.js'
+  },
+  // loaders section
+  module: {
+    rules: [
+      // JS Files
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env", ],
+        }
+      },
+      // Css files works with modules and plain css
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ],
+        include: /\.module\.css$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+        exclude: /\.module\.css$/
+      },
+    ]
+  },
+  // plugins section
+  plugins: [
+    // eslint plugin
+    new ESLintPlugin({
+      exclude: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, 'dist'),
+      ],
+      failOnError: true,
+      fix: true,
+      extensions: ['js', 'jsx', 'ts', 'tsx']
+    }),
 
-		new webpack.HotModuleReplacementPlugin(),
-	],
-	// Dev server
-	devServer: {
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  // Dev server
+  devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
